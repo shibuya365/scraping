e489503f654ddff2e89b893b3f749729159de2ba
shibuya365 <shibuya365days@gmail.com>
2020-11-28 15:20:42 +0900

    add news

--- main.go
+++ main.go
@@ -2,23 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"./conf" // 実装した設定ファイルパッケージの読み込み
+	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-
 	// 設定ファイルを読み込む
 	news, err := conf.ReadConfDB()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	var data conf.Data
-	data.Title = "あいうえお"
-	news = append(news, data)
-	err = conf.WriteConfDB(news)
+	// ニュースの読み込み
+	res, err := http.Get("https://news.yahoo.co.jp/")
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Fatalln(err)
 	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s\n", res.StatusCode, res.Status)
+	}
+
+	// タイトルのみの抜き出し
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	section := doc.Find(".topicsList_main a")
+
+	for i := 0; i < 8; i++ {
+		line := section.Eq(i)
+		text := line.Text()
+		fmt.Println(i+1, text)
+
+		// 新しいデータの作成
+		var data conf.Data
+		data.Title = text
+		news = append(news, data)
+		err = conf.WriteConfDB(news)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+	}
+
 }
