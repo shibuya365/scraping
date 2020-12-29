e2330b1e09c97a111f33c83470e6fe952c0138e2
shibuya365 <shibuya365days@gmail.com>
2020-12-29 21:40:32 +0900

    add goroutuine

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"example.com/get_yahoo_news/conf"
 	"github.com/PuerkitoBio/goquery"
@@ -18,69 +19,80 @@ var webs map[string]string = map[string]string{
 }
 
 func main() {
+	var wg sync.WaitGroup
 	// カテゴリごとに繰り返す
 	for cate, web := range webs {
-		// 設定ファイルを読み込む
-		news, err := conf.ReadConfDB(cate)
-		// もしファイルがなかったら
-		if err != nil {
-			news = []string{"もっと見る"}
-			fmt.Println(err.Error())
-		}
+		wg.Add(1)
+		go getCate(cate, web, &wg)
+	}
+	// 並列処理が終わるのをまつ
+	wg.Wait()
+}
 
-		// カテゴリのジャンルを表示へ
-		ans := "# " + cate + "\n"
+func getCate(cate string, web string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	// 設定ファイルを読み込む
+	news, err := conf.ReadConfDB(cate)
+	// もしファイルがなかったら
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-		// ニュースの読み込み
-		res, err := http.Get(web)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer res.Body.Close()
-		if res.StatusCode != 200 {
-			log.Fatalf("status code error: %d %s\n", res.StatusCode, res.Status)
-		}
+	// カテゴリのジャンルを表示へ
+	ans := "# " + cate + "\n"
 
-		// タイトルの部分の抜き出し
-		doc, err := goquery.NewDocumentFromReader(res.Body)
-		if err != nil {
-			log.Println(err)
-		}
+	// ニュースの読み込み
+	res, err := http.Get(web)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s\n", res.StatusCode, res.Status)
+	}
 
-		section := doc.Find(".sc-DNdyV a")
+	// タイトルの部分の抜き出し
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Println(err)
+	}
 
-		// 個別のニュースをチェック
-		section.Each(func(i int, line *goquery.Selection) {
+	section := doc.Find(".sc-DNdyV a")
 
-			// ニュースタイトルを取得
-			title := line.Text()
+	// 個別のニュースをチェック
+	section.Each(func(i int, line *goquery.Selection) {
 
-			// href属性を取得
-			attr, _ := line.Attr("href")
+		// ニュースタイトルを取得
+		title := line.Text()
 
-			// ニュースが既出が調べる
-			var app bool
-			for _, new := range news {
-				if title == new {
-					app = true
-					break
-				}
+		// href属性を取得
+		attr, _ := line.Attr("href")
+
+		// ニュースが既出が調べる
+		var app bool
+		for _, new := range news {
+			if title == "もっと見る" || title == new {
+				app = true
+				break
 			}
-			// もしなかったら
-			if !app {
-				// コンソールへ新しいニュースのタイトルのみ出力
-				ans += title + "		:" + attr + "\n"
-				// 新しいニュースタイトルを追加
-				news = append(news, title)
-			}
-		})
-		// ニュースを表示
-		fmt.Println(ans)
-
-		// データの保存
-		err = conf.WriteConfDB(cate, news)
-		if err != nil {
-			fmt.Println(err.Error())
 		}
+		// もしなかったら
+		if !app {
+			// コンソールへ新しいニュースのタイトルのみ出力
+			ans += title + "		:" + attr + "\n"
+			// 新しいニュースタイトルを追加
+			news = append(news, title)
+		}
+	})
+	// ニュースを表示
+	fmt.Println(ans)
+
+	// データの保存
+	if len(news) > 12 {
+		news = news[len(news)-19:]
+	}
+	err = conf.WriteConfDB(cate, news)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 }
